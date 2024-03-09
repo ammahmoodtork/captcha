7cd8d04be11b45085dff0657573f92b779c71b57
Amir Mahmoodtork <ammahmoodtork@gmail.com>
2024-03-09 16:41:10 +0330

    Hotfix for Backgroud image and Set random background

--- src/HeraCaptcha.php
+++ src/HeraCaptcha.php
@@ -12,7 +12,7 @@ class HeraCaptcha
 
         // $img = imagecreate(120, 36);
         try {
-            $photoAddress = (__DIR__ . "/assets/backgrounds/01.png");
+            $photoAddress = $this->getBackgroudImage();
             $img = imagecreatefrompng($photoAddress);
         } catch (\Throwable $th) {
             $img = imagecreate(120, 50);
@@ -26,7 +26,7 @@ class HeraCaptcha
             $color = $this->getfontColors();
             $textcolor = imagecolorallocate($img, $color[0], $color[1], $color[2]);
             imagettftext($img, 20, rand(-40, 40), ($i * (120 / $config['length'])) + 5, 28, $textcolor, (__DIR__ . "/assets/fonts/IRANSansWeb.ttf"), $char[$i]);
-            $txt .= $char[$i];
+            $txt .= $this->getchar($char[$i], $conf);
         }
         // imagettftext($img, 20, 0, 5, 28, $textcolor, (__DIR__ . "/assets/IRANSansWeb.ttf"), $txt);
         ob_start();
@@ -35,6 +35,13 @@ class HeraCaptcha
             "img" => "data:image/png;base64," . base64_encode(ob_get_clean()),
             "key" => Hash::make($txt)
         ];
+    }
+
+    private function getBackgroudImage()
+    {
+        $name = str_pad(rand(1, 12), 2, "0", STR_PAD_LEFT);
+        // dd((__DIR__ . "/assets/backgrounds/{$name}.png"));
+        return (__DIR__ . "/assets/backgrounds/{$name}.png");
     }
 
     private function getText($conf = 'default')
@@ -46,6 +53,12 @@ class HeraCaptcha
             $textArray[] = $characters[rand(0, count($characters) - 1)];
         }
         return $textArray;
+    }
+
+    private function getchar($char, $conf)
+    {
+        return $char;
+        dd(mb_convert_encoding(chr(ord('0') + 171), "UTF-8"), ord('۰'));
     }
 
     private function getfontColors($conf = 'default')
